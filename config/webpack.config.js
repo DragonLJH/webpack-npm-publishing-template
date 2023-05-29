4c48418611c9d2d282aa38bbb522d93d74342277
DragonLJH <1183665002@qq.com>
2023-05-29 11:26:40 +0800

    运行项目，运行时出现bug，修改依赖版本

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "development",
     entry: path.join(__dirname, "../src/index.js"),
     plugins: [
         new HtmlWebpackPlugin({
@@ -16,5 +17,11 @@ module.exports = {
         filename: "js/bundle.js",
         path: path.resolve(__dirname, "../dist"),
         clean: true,
-    }
+    },
+    devServer: {
+        compress: true,
+        host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
+        port: 3001, // 启动端口为 3001 的服务
+        open: false // 自动打开浏览器
+    },
 }
