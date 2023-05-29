995fe8991dabb0715180cb4276e13e4f40fe07da
DragonLJH <1183665002@qq.com>
2023-05-29 10:40:16 +0800

    管理输出 html，设置 HtmlWebpackPlugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,17 @@
 const path = require("path")
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: path.join(__dirname, "../src/index.js"),
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title: 'Development',
+            // 以 public/index.html 为模板创建文件
+            // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+            template: path.resolve(__dirname, "../public/index.html"),
+        }),
+    ],
     output: {
         filename: "js/bundle.js",
         path: path.resolve(__dirname, "../dist"),
