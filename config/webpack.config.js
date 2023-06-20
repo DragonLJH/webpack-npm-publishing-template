9dfa8e7a0619e86b75ebda99792f32db0bd5b820
DragonLJH <1183665002@qq.com>
2023-06-20 16:54:00 +0800

    添加vue、vue-router依赖，并修改配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path")
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { VueLoaderPlugin } = require('vue-loader');
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
     mode: "development",
@@ -16,12 +18,17 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "main.min.css" // 提取后的css的文件名
-        })
+        }),
+        new DefinePlugin({ __VUE_OPTIONS_API__: "true", __VUE_PROD_DEVTOOLS__: "false" }),
+        new VueLoaderPlugin(),
     ],
     module: {
         rules: [{
             test: /\.css$/i,
             use: ["style-loader", "css-loader"]
+        }, {
+            test: /\.vue$/i,
+            use: "vue-loader"
         }]
     },
     output: {
@@ -32,7 +39,7 @@ module.exports = {
     devServer: {
         compress: true,
         host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
-        port: 3001, // 启动端口为 3001 的服务
+        port: 3005, // 启动端口为 3001 的服务
         open: false // 自动打开浏览器
     },
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './index.css'
 // import install from "./components/index.js"
-import install from "webpack-npm-publishing-template"
+// import install from "webpack-npm-publishing-template"
+// install()
+import { createApp } from 'vue'
+// 从一个单文件组件中导入根组件
+import App from './App.vue'
+import router from './router/index';
 
-install()
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
